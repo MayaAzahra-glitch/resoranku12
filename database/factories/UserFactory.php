0d3b299ba68464b355e1d7bc8b124c2f816313a8
Maya Azahra <mazzahra947@gmail.com>
2026-06-02 17:39:48 +0700

    Add factories and seeders for items, categories, roles, and users

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -12,9 +12,7 @@ use Illuminate\Support\Str;
  */
 class UserFactory extends Factory
 {
-    /**
-     * The current password being used by the factory.
-     */
+   
     protected static ?string $password;
 
     /**
@@ -25,17 +23,16 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
+            'username' => fake()->unique()->UserName(),
             'password' => static::$password ??= Hash::make('password'),
-            'remember_token' => Str::random(10),
+            'fullname' => fake()->name(),
+            'email' => fake()->unique()->safeEmail(),
+            'phone' => fake()->phoneNumber(),
+            'role_id' => fake()->nuberBetween(1, 3),
         ];
     }
 
-    /**
-     * Indicate that the model's email address should be unverified.
-     */
+
     public function unverified(): static
     {
         return $this->state(fn (array $attributes) => [
